8bfcd88a026fc1dafc0c7a589ec0ce45f58394b4
edawg <muhammadridho5654@gmail.com>
2026-02-21 20:40:23 +0700

    chore: learn springboot

---
+++ src/main/java/com/edo/quickstart/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.edo.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController 
+public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
