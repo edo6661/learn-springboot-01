950753bf4250136b6bc335192ef9aa2b053b23a7
edawg <muhammadridho5654@gmail.com>
2026-02-22 16:04:29 +0700

    chore: env

--- src/main/java/com/edo/quickstart/HelloWorldController.java
+++ src/main/java/com/edo/quickstart/HelloWorldController.java
@@ -1,16 +1,18 @@
 package com.edo.quickstart;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController 
 public class HelloWorldController {
+	@Value("${HALO_TEST}")
+	private String haloTestEnv;
 
 	@GetMapping("/hello")
 	public String helloWorld() {
-		return "Hello Motherfucker";
+		return "Hello, user db: " + haloTestEnv;
 	}
 
 }
